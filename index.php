b87ac742f106c632990a27f22ddd62f74b55715e
intanpradnya <cmiwintan@gmail.com>
2025-12-20 21:54:04 +0800

    mengubah beberapa tombol menjadi ikon

--- index.php
+++ index.php
@@ -14,16 +14,30 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6 offset-md-3">
-          <h1 class="text-center">Aplikasi To Do List</h1>
+          <h1 class="text-center mb-4">Aplikasi To Do List</h1>
           
-          <div class="card">
+          <a href="#" class="btn btn-primary btn-sm"> 
+             <ion-icon name="add-outline"></ion-icon>
+          </a>
+
+          <div class="card mt-2">
             <div class="card-body">
               <div class="row">
-                <div class="col-md-9">Menyiapkan bahan makanan</div>
+                <div class="col-md-9">
+                  <ion-icon name="checkbox-outline" style="font-size: 20px;position:relative;
+                  top:5px;color:pink;"></ion-icon>
+                Menyiapkan bahan makanan
+              </div>
                 <div class="col-md-3">
-                  <a href ="#" class="btn btn-success btn-sm">SELESAI</a>
-                  <a href="#" class="btn btn-warning btn-sm">UBAH</a>
-                  <a href="#" class="btn btn-danger btn-sm">HAPUS</a>
+                  <a href ="#" class="btn btn-success btn-sm">
+                    <ion-icon name="checkmark-outline"></ion-icon>
+                  </a>
+                  <a href="#" class="btn btn-warning btn-sm">
+                    <ion-icon name="pencil-outline"></ion-icon>
+                  </a>
+                  <a href="#" class="btn btn-danger btn-sm">
+                    <ion-icon name="trash-outline"></ion-icon>
+                  </a>
                 </div>
               </div>
             </div>
@@ -33,6 +47,8 @@
     </div>
   </section>
   <script src="js/bootstrap.bundle.min.js"></script>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
