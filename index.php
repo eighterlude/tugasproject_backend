2934a888af4b44843966035b20c84931d72f2bc6
intanpradnya <cmiwintan@gmail.com>
2025-12-20 21:38:35 +0800

    menambahkan card body

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aplikasi To Do List</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
- <!--Main Section--> 
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Aplikasi To Do List</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <!--Main Section-->
   <section>
     <div class="container">
       <div class="row">
         <div class="col-md-6 offset-md-3">
           <h1 class="text-center">Aplikasi To Do List</h1>
+          
+          <div class="card">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-9">Menyiapkan bahan makanan</div>
+                <div class="col-md-3">
+                  <a href ="#" class="btn btn-success btn-sm">SELESAI</a>
+                  <a href="#" class="btn btn-warning btn-sm">UBAH</a>
+                  <a href="#" class="btn btn-danger btn-sm">HAPUS</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
-    <script src="js/bootstrap.bundle.min.js"></script>
-  </body>
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
 </html>
